490fdb29612d1f402a612fdd49acb14f457b0039
zbytspokojna <zbytspokojna@gmail.com>
2020-11-12 16:31:12 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,6 +3,9 @@ module.exports = {
 	description: 'Ban Command',
 	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
+		if(!message.mentions.users.first()) {
+			return message.channel.send(`You have not provided any arguments, ${message.author}!`);
+		}
 		const taggedUser = message.mentions.users.first();
 		message.channel.send(`You want to ban: ${taggedUser.username}`);
 		return;
